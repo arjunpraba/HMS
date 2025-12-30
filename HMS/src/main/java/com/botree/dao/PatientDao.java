3995af8ffa92d5fb141ed254cb20857fd7b9d637
sheryl <sheryl.gade@botree.co.in>
2025-12-30 15:52:04 +0530

    new

--- HMS/src/main/java/com/botree/dao/PatientDao.java
+++ HMS/src/main/java/com/botree/dao/PatientDao.java
@@ -1,5 +1,74 @@
 package com.botree.dao;
 
-public class PatientDao {
+import java.util.Collections;
+import java.util.List;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+import com.botree.model.Patient;
+
+@Repository
+public class PatientDao {
+	
+	@Autowired
+	 SessionFactory sessionFactory;
+	
+	  
+	  public boolean register(Patient patient) {
+			boolean flag=false;
+			var session=sessionFactory.openSession();
+			var tran=session.beginTransaction();
+			
+			try {
+				session.persist(patient);
+				flag=true;
+				tran.commit();
+				
+			}catch(Exception e)
+			{
+				tran.rollback();
+				e.printStackTrace();
+				flag=false;
+			}
+			
+			session.close();
+			
+			return flag;
+		}
+	  
+	  
+	  
+	  public List<Patient> searchByName(String searchName) {
+		    if (searchName == null || searchName.trim().isEmpty()) {
+		        return Collections.emptyList();
+		    }
+
+		    String searchParam = "%" + searchName.trim().toLowerCase() + "%";
+
+		    try (var session = sessionFactory.openSession()) {
+		        var query = session.createQuery(
+		            "from Patient p where lower(trim(p.firstName)) like :name or lower(trim(p.lastName)) like :name",
+		            Patient.class
+		        );
+		        query.setParameter("name", searchParam);
+		        return query.getResultList();
+		    }
+		}
+
+
+
+	  
+	  public List<Patient> searchById(int pid) {
+		    var session = sessionFactory.openSession();
+		    var query = session.createQuery("from Patient p where p.pid = :pid", Patient.class);
+		    query.setParameter("pid", pid);
+		    var list = query.getResultList();
+		    session.close();
+		    return list;
+		}
+		
 }
+
